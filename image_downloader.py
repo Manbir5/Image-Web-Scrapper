a23e7d1a1fb7e4ddb5d3a5898187134747377347
Manbir5 <72167868+Manbir5@users.noreply.github.com>
2021-11-08 19:21:47 -0500

    Add files via upload
    Added microservice python file

---
+++ image_downloader.py
@@ -0,0 +1,32 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+def imagedownloader(url,folder):
+    try:
+        os.mkdir(os.path.join(os.getcwd(), folder))
+    except:
+        print("Error, did not work. Please use another url")
+
+    os.chdir(os.path.join(os.getcwd(), folder))
+
+    request = requests.get(url)
+
+    soup = BeautifulSoup(request.text, "html.parser")
+
+    images = soup.find_all("img", width=True, height=True)
+
+    item = 0
+
+    for element in images:
+        alt = element["alt"]
+        if alt == "":
+            alt = folder + str(item)
+            item += 1
+        if int(element["width"]) > 90 and int(element["height"]) > 90:
+            link = element["src"]
+            with open(alt.replace(" ","_").replace("/","") + '.jpg','wb') as file:
+                image_request = requests.get(link)
+                file.write(image_request.content)
+                print("Writing: ", alt)
+            break
