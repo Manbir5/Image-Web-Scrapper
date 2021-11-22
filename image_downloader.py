4f0f6f6c3b36c02310f42a76862f144132f319cc
Manbir5 <72167868+Manbir5@users.noreply.github.com>
2021-11-21 21:59:31 -0500

    Added comments and structured code.

--- image_downloader.py
+++ image_downloader.py
@@ -6,9 +6,15 @@ from watchdog.events import PatternMatchingEventHandler
 import time
 from random import randrange
 
-def image_downloader(url,folder):
+def image_downloader(url, folder):
+    """This function downloads the image on the inputted url to a specified folder and returns the file's name or invalid
+    if it was not possible. This function takes two parameters. It takes a url in a string format and a folder name."""
+
+    # Need to store directory's location for when it changes due to the creation of a new folder.
 
     current_dir = os.getcwd()
+
+    # creates a new folder for the images
 
     try:
         os.mkdir(os.path.join(os.getcwd(), folder))
@@ -16,14 +22,19 @@ def image_downloader(url,folder):
         pass
 
     os.chdir(os.path.join(os.getcwd(), folder))
-
     request = requests.get(url)
-
     soup = BeautifulSoup(request.text, "html.parser")
+
+    # This function will only takes images that have defined width and height attributes.
 
     images = soup.find_all("img", width=True, height=True)
 
     item = (randrange(10000))
+
+    # The below loop goes through the images on the url (img tag) by their order of appearance and it will take the first
+    # image that is more than 90 x 90. It will take the alternative name of the image as a name for the file. If the
+    # alternative name has a .jpg or .png ending (like Wikipedia), it is removed. Image is saved as a .jpg in
+    # specified folder.
 
     for element in images:
         link = element["src"]
@@ -43,11 +54,17 @@ def image_downloader(url,folder):
             os.chdir(current_dir)
             return alt
 
+    # Changing pointer to stored directory.
+
     os.chdir(current_dir)
     invalid = "Invalid"
     return invalid
 
 def on_modified(event):
+    """This function is called whenever a modification is done on the specified image.txt file. It takes an event as its
+      only parameter. It gets the last line in the image.txt file and checks if it is a url. If it is a url, this function
+       will call the image_downloader function to save the image and get a file name. Lastly, it writes an error message
+       in the image.txt file if there are any errors.Otherwise, it writes the file name to the same file.  """
     current_dir = os.getcwd()
     line = ""
     with open(os.path.join(os.getcwd(), "images.txt"), "r") as file2:
@@ -68,6 +85,8 @@ def on_modified(event):
             file3.write("\n" + name)
         file3.close()
 
+# Creating variables to store parameters for PatternMatchingEventHandler and calling it.
+
 if __name__ =="__main__":
     patterns = ["*images.txt"]
     ignore_patterns = None
@@ -76,12 +95,15 @@ if __name__ =="__main__":
     my_event_handler = PatternMatchingEventHandler(patterns,ignore_patterns,ignore_directories,case_sensitive)
 
 
-my_event_handler.on_modified = on_modified
+# Making sure that the on_modified function is called whenever there is a change to the image.txt file.
 
+my_event_handler.on_modified = on_modified
 path = "."
 recursion = False
 my_observer = Observer()
 my_observer.schedule(my_event_handler, path, recursive = recursion)
+
+# Below code is to run the function continuously until it is manually stopped.
 
 my_observer.start()
 try:
